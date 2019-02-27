fa747abacdeff2d84ab3c9fddd8a6c973642dd7e
Mickae Truman <mtruman@nyit.edu>
2019-02-27 02:37:14 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
 # code here!
+class School
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def roster
+    @roster
+  end 
+  
+  #School.new ={}
+  
+  def add_student(name,grade)
+    roster[grade] ||= []
+    roster[grade] << name
+  end
+  
+  def grade(grade)
+    @grade = grade
+    @roster[grade]
+  end
+  
+  def sort
+    @roster.each{|grade,name| name.sort!}
+   end
+end 
